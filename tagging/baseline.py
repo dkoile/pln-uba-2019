e9a3c31b843ecc31617bbc65d6a448667de3385c
Daniel <koile.daniel@gmail.com>
2020-04-20 03:48:33 -0300

    TP1.2

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -34,12 +34,25 @@ class BadBaselineTagger:
 
 class BaselineTagger:
 
-    def __init__(self, tagged_sents, default_tag=None):
+    def __init__(self, tagged_sents, default_tag='nc0s000'):
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         default_tag -- tag for unknown words.
         """
-        # WORK HERE!!
+
+        self._default_tag = default_tag
+        self._tcount = defaultdict(dict)
+
+        for sent in tagged_sents:
+            for word in sent:
+                # Contando para cada palabra, cuantas veces aparece con cada etiqueta. Si los diccionarios no estan, los creo
+                if (word[0]) in self._tcount:
+                    if (word[1]) in self._tcount[word[0]]:
+                        self._tcount[word[0]][word[1]] += 1
+                    else:
+                        self._tcount[word[0]][word[1]] = 1
+                else:
+                    self._tcount[word[0]][word[1]] = 1
 
     def tag(self, sent):
         """Tag a sentence.
@@ -53,11 +66,20 @@ class BaselineTagger:
 
         w -- the word.
         """
-        # WORK HERE!!
+        print("tageando")
+        print(w)
+        if w in self._tcount:
+            itemMaxValue = max(self._tcount[w].items(), key=lambda x: x[1])
+            print('Max value in Dict: ', itemMaxValue[1])
+            print('Key With Max value in Dict: ', itemMaxValue[0])
+
+            return itemMaxValue[0]
+        else:
+            return self._default_tag
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        # WORK HERE!!
+        return (w not in self._tcount)
