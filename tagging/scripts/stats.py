8cbed0fb9d08596749dd2eaed96c8ca530433152
Daniel <koile.daniel@gmail.com>
2020-04-20 01:18:02 -0300

    Tp1.1 No pude hacer funcionar el docopt como estaba siendo usado, lo cambie por '<path>' que es la forma que entiendo correcta segun los manuales

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -9,8 +9,9 @@ Options:
 """
 from docopt import docopt
 from collections import defaultdict
-
+0
 from tagging.ancora import SimpleAncoraCorpusReader
+import collections
 
 
 class POSStats:
@@ -24,48 +25,94 @@ class POSStats:
         # WORK HERE!!
         # COLLECT REQUIRED STATISTICS INTO DICTIONARIES.
 
+        self._estadisticas = dict()
+        self._count = defaultdict(int)
+        self._countTag = defaultdict(int)
+        self._tcount = defaultdict(dict)
+        self._wcount = defaultdict(set)
+
+        sentences = 0
+        palabras = 0
+        for sent in tagged_sents:
+            sentences += 1 #Cantidad de oraciones
+            for word in sent:
+                palabras += 1 #Cantidad de ocurrencias de palabras
+                self._count[word[0]] += 1 #contando cuantas veces cada palabra aparece
+                self._countTag[word[1]] += 1 #contado de etiquetas (vocabulario de tags)
+                #print(word)
+
+                # Contando para cada etiqueta, cuantas veces aparece la palabra con esa etiqueta. Si los diccionarios no estan, los creo
+                if (word[1]) in self._tcount:
+                    if (word[0]) in self._tcount[word[1]]:
+                        self._tcount[word[1]][word[0]] += 1
+                    else:
+                        self._tcount[word[1]][word[0]] = 1
+                else:
+                    self._tcount[word[1]][word[0]] = 1
+
+                # Contando para cada palabra, los tags diferentes con que aparece. Si los diccionarios no estan, los creo
+                if (word[0]) in self._wcount:
+                    self._wcount[word[0]].add(word[1]) #agrega el tag al set de la palabra. si esta, el add ignora
+                else:
+                    self._wcount[word[0]] = {word[1]}
+
+        self._estadisticas["sentences"] = sentences
+        self._estadisticas["palabras"] = palabras
+        self._estadisticas["vocabulario"] = len(self._count) #Cantidad de palabras (vocabulario).
+        self._estadisticas["vocabularioDeTags"] = len(self._countTag)  # Cantidad de palabras (vocabulario).
+        self._estadisticas["tokens"] = sum(self._count.values())
+
     def sent_count(self):
         """Total number of sentences."""
-        # WORK HERE!!
+        return self._estadisticas["sentences"]
+
 
     def token_count(self):
         """Total number of tokens."""
-        # WORK HERE!!
+        return self._estadisticas["tokens"]
+
 
     def words(self):
         """Vocabulary (set of word types)."""
-        # WORK HERE!!
+        return self._estadisticas["palabras"]
 
     def word_count(self):
         """Vocabulary size."""
-        # WORK HERE!!
+        return self._estadisticas["vocabulario"]
+
 
     def word_freq(self, w):
         """Frequency of word w."""
-        # WORK HERE!!
+        return self._count[w]
 
     def unambiguous_words(self):
         """List of words with only one observed POS tag."""
-        # WORK HERE!!
+        return (ambiguous_words(self, 1))
 
     def ambiguous_words(self, n):
         """List of words with n different observed POS tags.
 
         n -- number of tags.
         """
-        # WORK HERE!!
+        ntag = []
+
+        for word in self._wcount:
+            if len(self._wcount[word]) == n:
+                ntag.append(word)
+
+        return ntag
 
     def tags(self):
         """POS Tagset."""
-        # WORK HERE!!
+        return(self._countTag.keys()) #(necesario que sea de tipo set?)
 
     def tag_count(self):
         """POS tagset size."""
-        # WORK HERE!!
+        return self._estadisticas["vocabularioDeTags"]
 
     def tag_freq(self, t):
         """Frequency of tag t."""
-        # WORK HERE!!
+        return(self._countTag[t])
 
     def tag_word_dict(self, t):
         """Dictionary of words and their counts for tag t."""
@@ -76,8 +123,16 @@ if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    corpus = SimpleAncoraCorpusReader(opts['-c'])
+
+    #corpus = SimpleAncoraCorpusReader(opts['-c'])  #No se porque no me esta cargando asi
+    corpus = SimpleAncoraCorpusReader(opts['<path>']) #por la documentacion que encontre, lo cambie a '<path>' para no hardcodearlo
+    # corpus = SimpleAncoraCorpusReader("ancora-3.0.1es")
     sents = corpus.tagged_sents()
+
+    count = defaultdict(int)
+
+
+
 
     # compute the statistics
     stats = POSStats(sents)
